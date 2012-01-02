35c79c1d7f261c6a20a9821c51d711b9db8a3ec8
David Persson <davidpersson@gmx.de>
2012-01-02 18:43:29 +0100

    Using mock in Invoice test; renaming test method.

--- tests/cases/models/InvoicesTest.php
+++ tests/cases/models/InvoicesTest.php
@@ -2,13 +2,44 @@
 
 namespace li3_freshbooks\tests\cases\models;
 
-use \li3_freshbooks\models\Invoices;
+use lithium\data\Connections;
+use li3_freshbooks\extensions\adapter\data\source\http\Freshbooks;
+use li3_freshbooks\models\Invoices;
 
 class InvoicesTest extends \lithium\test\Unit {
 
-	public function testWhetherDbQueryWorks() {
-	    $id = Invoices::find('first')->id;
-	    $this->assertTrue(!empty($id), 'Query did not pull data');
+	public $source;
+
+	protected $_backup = array();
+
+	protected $_testConfig = array(
+		'login' => 'test',
+		'password' => '123',
+		'socket' => 'li3_freshbooks\tests\mocks\data\source\http\MockFreshbooksSocket'
+	);
+
+	public function setUp() {
+		$this->_backup['connections'] = Connections::config();
+
+		Connections::reset();
+		$this->source = new Freshbooks(array('socket' => false));
+
+		Connections::config(array(
+			'freshbooks' => array(
+				'object' => &$this->source,
+				'adapter' => 'Freshbooks'
+			)
+		));
+	}
+
+	public function tearDown() {
+		Connections::reset();
+		Connections::config($this->_backup['connections']);
+	}
+
+	public function testFind() {
+	    $result = Invoices::find('first')->id;
+	    $this->assertTrue($id, 'Query did not pull data.');
 	}
 }
 
