669c17bff54cb2e0d2bc724d197130e8af8bba1c
spencer-leopold <speebo12@gmail.com>
2012-01-04 02:28:45 -0500

    'changes to testFind'

--- tests/cases/models/InvoicesTest.php
+++ tests/cases/models/InvoicesTest.php
@@ -2,13 +2,68 @@
 
 namespace li3_freshbooks\test\cases\models;
 
-use \li3_freshbooks\models\Invoices;
+use li3_freshbooks\models\Invoices;
 
 class InvoicesTest extends \lithium\test\Unit {
 
-	public function testWhetherDbQueryWorks() {
-	    $id = Invoices::find('first')->id;
-	    $this->assertTrue(!empty($id), 'Query did not pull data');
+	public $source;
+	
+	public $socket;
+
+	protected $_backup = array();
+
+	public function setUp() {
+		$this->_backup['connections'] = Connections::config();
+		Connections::reset();
+
+		$this->socket = new MockFreshbooksSocket();
+		$this->source = new Freshbooks(array(
+			'login' => 'test',
+			'password' => '123',
+			'socket' => $this->socket
+		));
+
+		Connections::config(array(
+			'freshbooks' => array(
+				'object' => &$this->source,
+				'adapter' => 'Freshbooks'
+			)
+		));
+	}
+
+	public function tearDown() {
+		Connections::reset();
+		Connections::config($this->_backup['connections']);
+	}
+
+	public function testFind() {
+
+		$response = array(
+			'HTTP/1.1 200 OK',
+			'Header: Value',
+			'Connection: close',
+			'Content-Type: application/xml;charset=UTF-8',
+			'',
+			'<?xml version="1.0"?>
+		       <response xmlns="http://www.freshbooks.com/api/" status="ok">
+		  	     <invoice>
+		    	    <invoice_id>1</invoice_id>
+		       	    <client_id>3</client_id>
+		    	    <contacts>
+		               <contact>
+		               <contact_id>0</contact_id>
+		        	   </contact>
+		    	    </contacts>
+				    ...
+				    ...
+				    '
+		);
+
+		$data = $response;
+		$this->socket->returnRead = implode("\r\n", $data);
+
+		$result = Invoices::find('all');
+		$this->assertTrue($result, 'Query did not pull data.');
 	}
 }
 
